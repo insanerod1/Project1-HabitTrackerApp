239f6e1d2761c4490a181cb5888b1b4fdb7f0b89
insanerod1 <rodg903@gmail.com>
2026-09-17 10:44:02 -0700

    pre data table

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habittracker/Controllers/HabitController.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -9,12 +8,11 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeState();
 }
 
-class _HomeState extends State<HomeScreen>
-{
-  final HabitController habitController = HabitController(); 
+class _HomeState extends State<HomeScreen> {
+  final HabitController habitController = HabitController();
 
   @override
-  void initState() {
+  void initState() {  //this guy is the function that changes the state of the homepage
     super.initState();
     habitController.addListener(() {
       setState(() {});
@@ -24,49 +22,39 @@ class _HomeState extends State<HomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: ListView.builder (
-          itemCount: habitController.getHabitList().length,
+        child: ListView.builder(
+          itemCount: habitController.getHabitList().length, //.getHabitList() returns a List
           itemBuilder: (context, index) {
-              return  Container(child: Text(habitController.getHabitList()[index].toString()));
+            return Container(     //this is what is built everytime the state is changed
+              child: Text(habitController.getHabitList()[index].toString()),
+            );
           },
-
-        )
+        ),
       ),
-      bottomNavigationBar: SafeArea(
+      bottomNavigationBar: SafeArea(                //the bottom of the page
         child: Padding(
           padding: EdgeInsets.all(5),
           child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    child: TextField(
-                      controller: textController,
-                    ),
-                  ),
-                ),
+            children: [
+              Expanded(
+                child: Container(child: TextField(controller: textController)),
+              ),
 
-                const SizedBox(width: 5, height: 10,),
-                ElevatedButton(
-                  onPressed: habitController.submitHabit,
-                  child: Text("ADD"), 
-                ),
+              const SizedBox(width: 5, height: 10),
+              ElevatedButton(
+                onPressed: habitController.submitHabit,
+                child: Text("ADD"),
+              ),
 
-                const SizedBox(width: 5, height: 10,),
-                ElevatedButton(
-                  onPressed: () {}, 
-                  child: Icon(Icons.add),
-                ),
-                
-                const SizedBox(width: 5, height: 10,),
-                ElevatedButton(
-                  onPressed: () {}, 
-                  child: Icon(Icons.add),
-                ),
-              ],
+              const SizedBox(width: 5, height: 10),
+              ElevatedButton(onPressed: () {}, child: Icon(Icons.add)),
+
+              const SizedBox(width: 5, height: 10),
+              ElevatedButton(onPressed: () {}, child: Icon(Icons.add)),
+            ],
           ),
         ),
       ),
     );
   }
-
 }
