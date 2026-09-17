e501dbd84595060e6e94ab6b8e3c07ad278c1b5d
insanerod1 <rodg903@gmail.com>
2026-09-17 12:22:16 -0700

    Data table finished

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habittracker/Controllers/HabitController.dart';
+import 'package:habittracker/widgets/datatable.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -22,15 +23,11 @@ class _HomeState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: ListView.builder(
-          itemCount: habitController.getHabitList().length, //.getHabitList() returns a List
-          itemBuilder: (context, index) {
-            return Container(     //this is what is built everytime the state is changed
-              child: Text(habitController.getHabitList()[index].toString()),
-            );
-          },
-        ),
-      ),
+        child: HabitTable(habits: habitController.getHabitList()),
+          ),   
+          
+        
+      
       bottomNavigationBar: SafeArea(                //the bottom of the page
         child: Padding(
           padding: EdgeInsets.all(5),
@@ -47,10 +44,10 @@ class _HomeState extends State<HomeScreen> {
               ),
 
               const SizedBox(width: 5, height: 10),
-              ElevatedButton(onPressed: () {}, child: Icon(Icons.add)),
+              ElevatedButton(onPressed: () {}, child: Text("Save")),
 
               const SizedBox(width: 5, height: 10),
-              ElevatedButton(onPressed: () {}, child: Icon(Icons.add)),
+              ElevatedButton(onPressed: () {}, child: Text("Load")),
             ],
           ),
         ),
---
+++ lib/widgets/datatable.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:habittracker/Models/Habits.dart';
+
+class HabitTable extends StatelessWidget {
+  
+  final List<Habit> habits;
+
+  const HabitTable({super.key, required this.habits,});
+  
+
+  @override build(BuildContext context)
+  {
+    return SingleChildScrollView(
+      child: DataTable(
+        columns: const [
+          DataColumn(label: Text('Habit')),
+          DataColumn(label: Text('Date')),
+        ], 
+        rows: habits.map((habit) {
+          return DataRow(
+            cells: [
+              DataCell(Text(habit.getItem())),
+              DataCell(Text(habit.getData())),
+            ]
+          );
+        }).toList(),
+         ),
+    );
+  }
+}
